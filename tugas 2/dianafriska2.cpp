52dbe3afbf96f549522bff8247fa4a93c08174a6
DianaFriskaAulia <156889167+DianaFriskaAulia@users.noreply.github.com>
2024-01-18 16:19:01 +0700

    Add files via upload

---
+++ tugas 2/dianafriska2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // contoh pengulangan for
+    for (int i = 10; i >=5; --i) {
+        std::cout << "iterasi ke-" << i << std::endl;
+    }
+
+    return 0;
+
+}
+
